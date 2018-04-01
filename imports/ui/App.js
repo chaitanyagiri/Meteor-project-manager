7340836d4cdb31290d252a84bafc140c26cbd7cc
Chaitanya Giri <girichaitanya11@gmail.com>
2018-04-02 03:45:30 +0530

    Added Some more features

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -7,6 +7,12 @@ import { Projects } from '../api/Projects.js';
 
 // App component - represents the whole app
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hideCompleted: false,
+        };     
+    }
     handleSubmit(event) {
         event.preventDefault();
      
@@ -21,8 +27,18 @@ class App extends Component {
         // Clear form
         ReactDOM.findDOMNode(this.refs.textInput).value = '';
     }
+    toggleHideCompleted(){
+        let checkstatus = this.state.hideCompleted;
+        this.setState({
+            hideCompleted: !checkstatus,
+        });
+    }
     renderProjects() {
-        return this.props.projects.map((project) => (
+        let filteredProjects = this.props.projects;
+        if (this.state.hideCompleted) {
+            filteredProjects = filteredProjects.filter(project => !project.checked);
+        }
+        return filteredProjects.map((project) => (
             <Project key={project._id} project={project} />
         ));
     }
@@ -30,7 +46,16 @@ class App extends Component {
         return (
             <div className="container">
             <header>
-                <h1>ProjectManager</h1>
+                <h1>ProjectManager ({this.props.uncompletedProjects} Uncompleted)</h1>
+            <label className="hide-completed">
+                <input
+                    type="checkbox"
+                    readOnly
+                    checked={this.state.hideCompleted}
+                    onClick={this.toggleHideCompleted.bind(this)}
+                />
+                Hide Completed Tasks
+            </label>
                 <form className="new-project" onSubmit={this.handleSubmit.bind(this)} >
                     <input
                     type="text"
@@ -43,7 +68,6 @@ class App extends Component {
                     />
                 </form>
             </header>
-
             <ul>
                 {this.renderProjects()}
             </ul>
@@ -55,6 +79,7 @@ class App extends Component {
 export default withTracker(() => {
     return {
       projects: Projects.find({}, { sort: { createdAt: 1 } }).fetch(),
+      uncompletedProjects: Projects.find({ checked: { $ne: true } }).count()
     };
   })(App);
   
